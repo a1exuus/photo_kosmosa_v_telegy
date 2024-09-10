4200412c92b1dbc3830e2cd79e7f87b614f65e2c
a1exuus <162327737+a1exuus@users.noreply.github.com>
2024-09-10 16:47:56 +0300

    publish photo to Telegram channel

--- telegram_chat_bot.py
+++ telegram_chat_bot.py
@@ -7,4 +7,4 @@ load_dotenv()
 bot_token = os.getenv('TG_BOT_TOKEN')
 bot = telegram.Bot(token=bot_token)
 chat_id = '-1002265052157'
-bot.send_message(chat_id=chat_id, text='Привет, салам алейкум лееееееееееее')
+bot.send_document(chat_id=chat_id, document=open('image/nasa_apod_0.png', 'rb'))
